d303a4da917f46da3cff9b344c2d640831fe6ab9
nilesh-singh-2304 <shyaam2304@gmail.com>
2025-01-11 21:38:28 +0530

    added auth

---
+++ pages/api/signin.ts
@@ -0,0 +1,37 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import User from "@/model/user";
+import connectDB from "@/middleware/mongoose";
+var jwt = require("jsonwebtoken");
+
+type Data = {
+  name: string;
+};
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method == "POST") {
+    const user = await User.findOne({ email: req.body.email });
+    
+    if (user) {
+      if (
+        req.body.email == user.email &&
+        req.body.password == user.password
+      ) {
+        var token = jwt.sign(
+          { success: true, email: user.email, name: user.name },
+          "Secret you wanna give",
+          { expiresIn: "1d" }
+        );
+        res.status(200).json({ success: true, token, email: req.body.email });
+      } else {
+        res.status(200).json({ success: false, error: "Invalid Credentials" });
+      }
+    } else {
+      res.status(200).json({ success: false, error: "User Not Found" });
+    }
+  } else {
+    res.status(400).json({ error: "This method is not allowed" });
+  }
+};
+
+export default connectDB(handler);
---
+++ pages/api/signup.ts
@@ -0,0 +1,28 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import User from "@/model/user"
+import connectDB from "@/middleware/mongoose";
+
+type Data = {
+  name: string;
+};
+
+const handler = async (
+    req: NextApiRequest,
+    res: NextApiResponse,
+  )=> {
+    if(req.method == 'POST'){
+        // const {name,email} = req.body
+        let u = new User({email:req.body.email, password:req.body.password})
+        await u.save()
+        res.status(200).json({ success:true });
+        
+    }
+    else{
+        res.status(400).json({ error : false });
+    }
+      
+  }
+  
+
+export default connectDB(handler)
