7b8d60419fdb15f7369449347cdbfc38056b609a
nilesh-singh-2304 <shyaam2304@gmail.com>
2025-01-11 22:32:53 +0530

    final

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -16,7 +16,7 @@ const Navbar = () => {
     <div>
       <div className="navbar bg-base-100">
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+
         </div>
         <div className="flex-none">
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Index = () => {
   return (
-    <div>
-      
+    <div className='h-screen'>
+      <div className="bg-gradient-to-b h-screen from-orange-600 via-orange-500 to-orange-400 px-6 sm:py-20 py-10 font-[sans-serif]">
+      <div className="max-w-screen-xl mx-auto text-center text-white">
+        <h1 className="text-5xl max-sm:text-3xl font-extrabold leading-tight mb-6">Welcome to Sploot</h1>
+        <p className="text-lg mb-12">This is an Assignment website for Sploot.</p>
+        <Link href="/signup"><button className="px-6 mx-2 py-3 rounded-full bg-white text-blue-900 font-semibold hover:bg-blue-100 transition duration-300 ease-in-out">Login</button></Link>
+        <Link href="/blogs"><button className="px-6 mx-2 py-3 rounded-full bg-white text-blue-900 font-semibold hover:bg-blue-100 transition duration-300 ease-in-out">Blogs</button></Link>
+      </div>
+    </div>
     </div>
   )
 }
