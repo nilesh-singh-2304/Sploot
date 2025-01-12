1ac2ff2eaadec14a6e04867c092fcf9f02a190c6
nilesh-singh-2304 <shyaam2304@gmail.com>
2025-01-12 13:05:07 +0530

    new

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import NextTopLoader from 'nextjs-toploader';
+// import NextTopLoader from 'nextjs-toploader';
 
 export default function Document() {
   return (
